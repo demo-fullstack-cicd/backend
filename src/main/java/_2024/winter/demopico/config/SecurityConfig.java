808e90976ebaabaca86043a67d4082f03810374b
junho <jh102328@khu.ac.kr>
2025-02-18 00:44:59 +0900

    .

--- src/main/java/_2024/winter/demopico/config/SecurityConfig.java
+++ src/main/java/_2024/winter/demopico/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
         //경로별 인가 작업
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/h2-console/**", "/login", "/register/**", "/reissue", "/images/**").permitAll()
+                        .requestMatchers("/h2-console/**", "/login", "/register/**", "/reissue").permitAll()
                         .anyRequest().authenticated());
 
         http
