e91a4dab2a86c87628198379259f52b4d725b160
junho <jh102328@khu.ac.kr>
2025-02-17 20:47:40 +0900

    update github acitons

--- src/main/java/_2024/winter/demopico/domain/user/controller/RegisterController.java
+++ src/main/java/_2024/winter/demopico/domain/user/controller/RegisterController.java
@@ -9,15 +9,13 @@ import _2024.winter.demopico.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/register")
+//@CrossOrigin(origins = "*", methods = RequestMethod.POST)
 public class RegisterController {
 
     private final EmailService emailService;
