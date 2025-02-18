97e7df6cb6107c48cb67e6c36cc20e3212252e93
junho <jh102328@khu.ac.kr>
2025-02-18 16:50:46 +0900

    Update Health Check

--- src/main/java/_2024/winter/demopico/config/SecurityConfig.java
+++ src/main/java/_2024/winter/demopico/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
         //경로별 인가 작업
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/h2-console/**", "/login", "/register/**", "/reissue").permitAll()
+                        .requestMatchers("/h2-console/**", "/login", "/register/**", "/reissue", "/health").permitAll()
                         .anyRequest().authenticated());
 
         http
---
+++ src/main/java/_2024/winter/demopico/domain/health/HealthCheckController.java
@@ -0,0 +1,13 @@
+package _2024.winter.demopico.domain.health;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("/api/health")
+    public ResponseEntity<String> healthCheck(){
+        return ResponseEntity.ok().body("OK");
+    }
+}
