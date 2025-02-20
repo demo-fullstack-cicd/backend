f83815575809675c1991f524a4b4ada25d77c28b
Jung Junho <129667716+juhhoho@users.noreply.github.com>
2025-02-20 14:59:32 +0900

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -eux
+
+# 루트 권한으로 실행
 sudo -s
-# Docker 설치
+
+# Docker 설치 및 실행
 dnf install -y docker
 systemctl enable --now docker
 usermod -aG docker ec2-user
@@ -11,7 +14,7 @@ curl -L "https://github.com/docker/compose/releases/latest/download/docker-compo
 chmod +x /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
-# CodeDeploy Agent 설치
+# AWS CodeDeploy Agent 설치
 dnf install -y ruby wget
 cd /home/ec2-user
 wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
