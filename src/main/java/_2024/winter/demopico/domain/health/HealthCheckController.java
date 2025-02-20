3374a1cbcda9e8556877cf010b18a894ccc4c0cf
Jung Junho <129667716+juhhoho@users.noreply.github.com>
2025-02-20 18:35:04 +0900

    Update HealthCheckController.java

--- src/main/java/_2024/winter/demopico/domain/health/HealthCheckController.java
+++ src/main/java/_2024/winter/demopico/domain/health/HealthCheckController.java
@@ -1,6 +1,7 @@
 package _2024.winter.demopico.domain.health;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
