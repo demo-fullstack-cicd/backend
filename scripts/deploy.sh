d6ff4e2d38be6421ccdca20f460007740865c9bd
Jung Junho <129667716+juhhoho@users.noreply.github.com>
2025-02-21 10:28:00 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 set -eux
 
-# S3에서 .env 파일 다운로드 (EC2 홈 디렉토리로 다운로드)
-aws s3 cp s3://${S3_BUCKET_NAME}/.env /home/ec2-user/.env
-
-# 다운로드한 .env 파일의 권한을 ec2-user에게 부여
-sudo chown ec2-user:ec2-user /home/ec2-user/.env
-
-# 환경 변수 로드
-export $(cat /home/ec2-user/.env | xargs)
-
 # Docker 로그인
 docker login -u $(DOCKER_USERNAME) -p $(DOCKER_PASSWORD)
 
